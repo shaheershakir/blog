5cf7ff3b1101ca0d6151cd4ed0078177211fad36
Shaheer Shakir <shaheershakir22@gmail.com>
2023-09-14 23:40:53 +0530

    sort and filter pages

--- src/js/utils.js
+++ src/js/utils.js
@@ -15,3 +15,41 @@ export function slugify(text) {
         timeZone: 'UTC',
     });
   } 
+
+  export function formatBlogPosts(posts, {
+    filterOutDrafts = true,
+    filterOutFuturePosts = true,
+    sortByDate = true,
+    limit = undefined
+  } = {}) {
+
+    const filterPosts = posts.reduce((acc, post) => {
+      const {date, draft} = post.frontmatter;
+      //filter out drafts if true
+      if(filterOutDrafts && draft) return acc;
+
+      //filter out future posts if true
+      if(filterOutFuturePosts && new Date(date) > new Date()) return acc;
+      
+      //add post to array
+      acc.push(post);
+
+      return acc;
+    }, []);
+
+    //sort by date if true
+    if(sortByDate) {
+      filterPosts.sort((a, b) => {
+        return new Date(b.frontmatter.date) - new Date(a.frontmatter.date);
+      });
+    } else {
+      filterPosts.sort((a, b) => Math.random() - 0.5);
+      };
+
+    //limit posts if true
+      if(typeof limit === 'number') {
+        filterPosts: filterPosts.slice(0, limit);
+      }
+      return filterPosts;
+
+  }
