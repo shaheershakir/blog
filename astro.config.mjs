d032acdc861e34085e4008aa967d054793a6eea7
Shaheer Shakir <shaheershakir22@gmail.com>
2023-09-15 17:54:29 +0530

    tested integration (react/tailwind)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-blog-cip.netlify.app',
-  integrations: [sitemap()]
+  integrations: [sitemap(), react(), tailwind()]
 });
